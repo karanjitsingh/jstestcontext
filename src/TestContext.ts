a2c23e110450d344ee7e34ca2b99110525acc702
Karan Jit Singh <karsin@microsoft.com>
2019-01-27 15:33:08 +0530

    upload attachments

--- src/TestContext.ts
+++ src/TestContext.ts
@@ -1,5 +1,10 @@
+import { Md5 } from './Utils/MD5';
+// import * as fs from 'fs';
+// import * as path from 'path';
+
 // tslint:disable:no-invalid-this
 // tslint:disable:no-arg
+// tslint:disable:no-banned-terms
 
 const testResultsEnvVar = 'JSTEST_RESULTS_DIRECTORY';
 
@@ -21,12 +26,39 @@ try {
     // how to log?
 }
 
+function getTestGuid(testName: string): string {
+    const hash = new Md5();
+    hash.appendStr(testName);
+    return hash.getGuid();
+}
+
+function callerMatch(method: any, caller: any, depth: number): boolean {
+    if (method === caller) {
+        return true;
+    } else if (!caller) {
+        return false;
+    }
+
+    let nestedCaller = caller;
+    for (let i = 1; i <= depth; i++) {
+        nestedCaller = nestedCaller.caller;
+
+        if (!nestedCaller) {
+            return false;
+        }
+
+        if (nestedCaller === method) {
+            return true;
+        }
+    }
+}
+
 export namespace TestContext {
-    export function getCurrentTestName() {
+    export function getCurrentTestName(): string | null {
         if (itOverrideSuccess) {
             // Jasmine/Jest
             for (let i = 0; i < itList.length; i++) {
-                if (itList[i][2] === <any>getCurrentTestName.caller ) {
+                if (callerMatch(itList[i][2], <any>getCurrentTestName.caller, 5)) {
                     
                     const spec = itList[i][0];
 
@@ -50,14 +82,38 @@ export namespace TestContext {
         }
     }
 
-    export function getAttachmentDirectory(): string | null {
-        if (process.env[testResultsEnvVar]) {
-            return process.env[testResultsEnvVar];
-        }
+    export function getTestAttachmentDirectory(): string | null {
+        // const testResultsDirectory = process.env[testResultsEnvVar];
+        
+        // if (!testResultsDirectory) {
+        //     return null;
+        // }
+
+        // const testName = this.getCurrentTestName();
+
+        // if (!testName) {
+        //     return null;
+        // }
+
+        // try {
+        //     const testHash = getTestGuid(testName);
+        //     const testFolder = path.join(testResultsDirectory, testHash);
+
+        //     if (!fs.existsSync(testFolder)) {
+        //         fs.mkdirSync(testFolder);
+        //     }
+
+        //     return testFolder;
+        // } catch (e) {
+        //     // how to log?
+        //     return null;
+        // } 
         return null;
     }
 
     export function uploadAttachment(path: string): boolean {
+        // const testAttachmentDirectory = getTestAttachmentDirectory();
+        
         return false;
     }
 }
